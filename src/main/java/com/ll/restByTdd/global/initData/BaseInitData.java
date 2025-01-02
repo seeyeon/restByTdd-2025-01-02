c10c84e4565217ef40380da57992a1361145d0b6
seeyeon <gulf72pr@swu.ac.kr>
2025-01-02 10:17:53 +0900

    baseInitDataApplicationRunner

---
+++ src/main/java/com/ll/restByTdd/global/initData/BaseInitData.java
@@ -0,0 +1,31 @@
+package com.ll.restByTdd.global.initData;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.transaction.annotation.Transactional;
+@Configuration
+@RequiredArgsConstructor
+public class BaseInitData {
+    @Autowired
+    @Lazy
+    private BaseInitData self;
+    @Bean
+    public ApplicationRunner baseInitDataApplicationRunner() {
+        return args -> {
+            self.work1();
+            self.work2();
+        };
+    }
+    @Transactional
+    public void work1() {
+        System.out.println("work1");
+    }
+    @Transactional
+    public void work2() {
+        System.out.println("work2");
+    }
+}
