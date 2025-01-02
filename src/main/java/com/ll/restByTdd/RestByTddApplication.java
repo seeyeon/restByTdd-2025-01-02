fbd6bd317e69a62e0e4026f53ca043167f07941b
seeyeon <gulf72pr@swu.ac.kr>
2025-01-02 09:54:16 +0900

    setting

---
+++ src/main/java/com/ll/restByTdd/RestByTddApplication.java
@@ -0,0 +1,14 @@
+package com.ll.restByTdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestByTddApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RestByTddApplication.class, args);
+	}
+
+}
